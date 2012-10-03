6f5b619b4d64bf0995299aaafb895b337bcf3995
Amitai Burstein <amitai@gizra.com>
2012-10-03 21:36:15 +0200

    First completely off commit.

---
+++ lib/Drupal/entityreference/Plugin/Type/Selection/SelectionPluginManager.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\entityreference\Plugin\Type\Selection\SelectionPluginManager.
+ */
+
+namespace Drupal\entityreference\Plugin\Type\Selection;
+
+use Drupal\Component\Plugin\PluginManagerBase;
+use Drupal\Core\Plugin\Discovery\CacheDecorator;
+use Drupal\Core\Plugin\Discovery\AnnotatedClassDiscovery;
+
+/**
+ * Plugin type manager for field widgets.
+ */
+class SelectionPluginManager extends PluginManagerBase {
+
+  /**
+   * Overrides Drupal\Component\Plugin\PluginManagerBase:$defaults.
+   */
+  protected $defaults = array(
+    'force_enabled' => FALSE,
+  );
+
+  /**
+   * The cache id used for plugin definitions.
+   *
+   * @var string
+   */
+  protected $cache_key = 'entityreference_selection';
+
+  /**
+   * Constructs a WidgetPluginManager object.
+   */
+  public function __construct() {
+    $this->baseDiscovery = new AnnotatedClassDiscovery('entityreference', 'selection');
+    $this->discovery = new CacheDecorator($this->baseDiscovery, $this->cache_id, $this->cache_bin);
+  }
+
+  /**
+   * Overrides Drupal\Component\Plugin\PluginManagerBase::getDefinition().
+   *
+   * @todo Remove when http://drupal.org/node/1778942 is fixed.
+   */
+  public function getDefinition($plugin_id) {
+    $definition = $this->discovery->getDefinition($plugin_id);
+    if (!empty($definition)) {
+      $this->processDefinition($definition, $plugin_id);
+      return $definition;
+    }
+  }
+
+}
